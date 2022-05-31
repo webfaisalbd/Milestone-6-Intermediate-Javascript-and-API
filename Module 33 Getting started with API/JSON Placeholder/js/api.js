e8f4c0a2b6eab0b3bfe28799dcf46dd60f535926
webfaisalbd <web.faisal.bd@gmail.com>
2022-05-31 19:05:58 +0600

    module 33.4

--- Module 33 Getting started with API/JSON Placeholder/js/api.js
+++ Module 33 Getting started with API/JSON Placeholder/js/api.js
@@ -3,3 +3,15 @@ function loadData(){
     .then(res => res.json())
     .then(data => console.log(data))
 }
+
+
+function loadUsers(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res => res.json())
+    .then(data => displayUser(data))
+    
+}
+
+function displayUser(data) {
+    console.log(data)
+}
