21f5fa21c43761497e451a6f3debcc5e56e79cca
webfaisalbd <web.faisal.bd@gmail.com>
2022-05-31 19:40:06 +0600

    module 33.5

--- Module 33 Getting started with API/JSON Placeholder/js/api.js
+++ Module 33 Getting started with API/JSON Placeholder/js/api.js
@@ -13,5 +13,10 @@ function loadUsers(){
 }
 
 function displayUser(data) {
-    console.log(data)
+  const ul = document.getElementById('users');
+  for(const user of data){
+    const li = document.createElement('li');
+    li.innerText = `Name: ${user.name} : Email: ${user.email}` ;
+    ul.appendChild(li);
+  }
 }
