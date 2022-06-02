bbe320c5a521f50c4e1a3da1976303debde79b93
webfaisalbd <web.faisal.bd@gmail.com>
2022-06-03 02:14:24 +0600

    module 34-7 Create dynamic url based on click and display data

--- Module 34 API Examples and edge cases/MealDB/app.js
+++ Module 34 API Examples and edge cases/MealDB/app.js
@@ -1,7 +1,7 @@
 const searchFood = () => {
     const searchField = document.getElementById('search-field');
     const searchText = searchField.value;
-    console.log(searchText);
+    // console.log(searchText);
 
     searchField.value = '';
 
@@ -16,19 +16,50 @@ const searchFood = () => {
 const displayMeals = (meals) => {
     const searchResult = document.getElementById('search-result');
 
-    meals.forEach(meal =>{
-        console.log(meal);
+    meals.forEach(meal => {
+        // console.log(meal);
         const div = document.createElement('div');
         div.classList.add('col');
         div.innerHTML = `
-        <div class="card">
+        <div onclick="loadMealDetail(${meal.idMeal})" class="card">
             <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
             <div class="card-body">
               <h5 class="card-title">${meal.strMeal}</h5>
-              <p class="card-text">${meal.strInstructions.slice(0,200)}.....</p>
+              <p class="card-text">${meal.strInstructions.slice(0, 200)}.....</p>
             </div>
           </div>
         `;
         searchResult.appendChild(div);
     })
+}
+
+const loadMealDetail = (meal) => {
+    // console.log(meal);
+    const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${meal}`;
+
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayMealDetails(data.meals[0]))
+}
+
+const displayMealDetails = meal => {
+    // console.log(meal);
+
+    const singleCardDetails = document.getElementById('single-card-details');
+
+    const div = document.createElement('div');
+    div.classList.add('card');
+    div.style.width= '48rem';
+    div.style.marginBottom= '20px';
+
+    div.innerHTML = `
+    <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">${meal.strMeal}</h5>
+      <p class="card-text">${meal.strInstructions}</p>
+      <a href="${meal.strYoutube}" target="_blank" class="btn btn-info">Go Youtube</a>
+    </div>
+
+    `;
+    singleCardDetails.appendChild(div);
 }
